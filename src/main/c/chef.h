0d45ece6b364cc827a4667326e19bf464978c390
exqudens <exqudens@gmail.com>
2022-09-09 01:06:19 -0700

    step 3

--- src/main/c/chef.h
+++ src/main/c/chef.h
@@ -1,9 +1,6 @@
 #ifndef CHEF_H
 #define CHEF_H
 
-#include <uchar.h>
-#include <stdint.h>
-
 unsigned int chefMakeOrder(const char* name);
 
 #endif // CHEF_H
--- src/main/c/waiter.h
+++ src/main/c/waiter.h
@@ -1,9 +1,6 @@
 #ifndef WAITER_H
 #define WAITER_H
 
-#include <uchar.h>
-#include <stdint.h>
-
 unsigned int waiterMakeOrder(const char* name);
 
 #endif // WAITER_H
